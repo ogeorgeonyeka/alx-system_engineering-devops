2730191ddad3f6b3445cd4d622a50d2e76f7479e
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-11-28 07:30:06 -0800

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Creates a file in /tmp
 
 file { '/tmp/school':
-	constant => 'I love Puppet'.
-	mode => '0744'.
-	owner => 'www-data'.
-	group => 'www-data'.
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+	content => 'I love Puppet'
 }
+
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kill process killmenow
 
-exec { 'pkill':
-	command => 'pkill killmenow'.
-	provider => 'shell'.
+exec { 'pkill killmenow':
+	path => '/usr/bin:/usr/sbin:/bin'
 }
+
