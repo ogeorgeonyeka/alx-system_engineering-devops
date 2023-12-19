c9200fb60448560a544edd4aa1a5ce13d39853a1
george okoronkwo <ogeorgeonyeka@gmail.com>
2023-12-18 22:29:43 -0800

    0x0B-ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,17 +3,13 @@
 
 
 file { '/etc/ssh/ssh_config':
-	ensure => presents,
-}
+	ensure => present,
 
-file_line { 'Turn off passwd auth':
-	path => '/etc/ssh/ssh_config',
-	line => 'PasswordAuthentication',
-	match => '^#PasswordAuthentication',
-}
+content =>"
 
-file_line { 'Declare identity file':
-	path => '/etc/ssh/ssh_config',
-	line => 'IdentityFile ~/.ssh/school',
-	match => '^#IdentityFile',
+	# ssh client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
 }
